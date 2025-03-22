bafba7e0b0c3ba829160ce0da7a5c4387491e711
Peter3255 <YOUR_EMAIL>
2025-03-21 20:13:23 -0700

    Changes committed

--- react-with-vite/src/components/imports/pages/Home.js
+++ react-with-vite/src/components/imports/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import RegisterForm from '../RegisterForm';
 
 export default function Home() {
     return (
--- react-with-vite/src/server/server.js
+++ react-with-vite/src/server/server.js
@@ -1,15 +1,15 @@
+const express = require('express');
 
-const express = require('express')
+require('dotenv').config({ path: "./config.env" });
+const PORT = process.env.PORT || 8080;
 
 // create express instance
 const app = express();
 app.use(express.json());
 
+// routes
+app.use('/api', require('./router/router'));
 
-app.get('/', (req, res)=>{
-    res.send("Server Request")
-})
-
-app.listen(4000, ()=> {
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:4000`);
 })
